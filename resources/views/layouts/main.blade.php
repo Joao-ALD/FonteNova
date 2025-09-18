ca7402ed27ebfc9490e0743fb5c11ac8f76fadb9
JoaoALD <joaovictorald@gmail.com>
2025-09-18 14:33:25 -0300

    temporáriamente-add-bootstrapVirtual

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,15 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
     <title>FonteNova</title>
 </head>
+
 <body>
 
     <nav class="navbar navbar-expand-lg  bs-primary">
         <!-- //- 'container-fluid' faz com que o conteúdo da barra ocupe toda a largura da tela. -->
         <div class="container-fluid text-white bg-nav nav-h">
             <!-- //- 'navbar-brand' é usado para o nome do site ou logo. -->
-            <a class="navbar-brand" href="index "><img src="assets\img\LogoReduzida.svg" alt="LogoFonteNova"></a>
+            <a class="navbar-brand" href="#"><img src="assets\img\LogoReduzida.svg" alt="LogoFonteNova"></a>
 
             <!-- //- Este é o botão "hambúrguer" que aparece em telas pequenas. -->
             <!-- //- 'data-bs-toggle="collapse"' e 'data-bs-target' o conectam ao nav que deve ser mostrado/escondido. -->
@@ -30,27 +33,27 @@
                     <!-- //- Item da lista de navegação. -->
                     <li class="nav-item">
                         <!-- //- Link simples para a página "Home". 'active' o deixa destacado. -->
-                        <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
                     </li>
                     <!-- //- Item da lista de navegação. -->
                     <li class="nav-item">
                         <!-- //- Link simples 'active' o deixa destacado. -->
-                        <a class="nav-link active text-white" aria-current="page" href="index.php">Sobre</a>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Sobre</a>
                     </li>
                     <!-- //- Item da lista de navegação. -->
                     <li class="nav-item">
                         <!-- //- Link simples 'active' o deixa destacado. -->
-                        <a class="nav-link active text-white" aria-current="page" href="index.php">Galeria</a>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Galeria</a>
                     </li>
                     <!-- //- Item da lista de navegação. -->
                     <li class="nav-item">
                         <!-- //- Link simples 'active' o deixa destacado. -->
-                        <a class="nav-link active text-white" aria-current="page" href="index.php">Mapa</a>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Mapa</a>
                     </li>
                     <!-- //- Item da lista de navegação. -->
                     <li class="nav-item">
                         <!-- //- Link simples 'active' o deixa destacado. -->
-                        <a class="nav-link active text-white" aria-current="page" href="index.php">Curso</a>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Curso</a>
                     </li>
                 </ul>
             </div>
@@ -58,10 +61,11 @@
     </nav>
 
     <section class="container">
-    @yield('content')
+        @yield('content')
     </section>
 
     <script src="assets/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
 </html>

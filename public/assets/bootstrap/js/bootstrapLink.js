baca7b5d88575a6589ed83a65f0efb23ecb24b00
JoaoALD <joaovictorald@gmail.com>
2025-09-18 15:31:04 -0300

    corrigindo bootstrap local

---
+++ public/assets/bootstrap/js/bootstrapLink.js
@@ -0,0 +1,19 @@
+/* Bootstrap Bundle */
+import 'bootstrap.bundle.js';
+import 'bootstrap.bundle.min.js';
+
+/* Bootstrap ESM */
+import 'bootstrap.esm.js';
+import 'bootstrap.esm.min.js';
+
+/* Bootstrap */
+import 'bootstrap.js';
+import 'bootstrap.min.js';
+
+/* Mapas dos arquivos JavaScript */
+import 'bootstrap.bundle.js.map';
+import 'bootstrap.bundle.min.js.map';
+import 'bootstrap.esm.js.map';
+import 'bootstrap.esm.min.js.map';
+import 'bootstrap.js.map';
+import 'bootstrap.min.js.map';

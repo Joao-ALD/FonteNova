d183b4d7a64c57c1454917080ebde07f147b88c6
JoaoALD <joaovictorald@gmail.com>
2025-09-18 15:07:01 -0300

    .

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,8 +9,12 @@
 
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
+    {{-- Bootsrap CSS (site) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
     {{-- Bootstrap CSS (local) --}}
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.css') }}">
 
     {{-- CSS customizado do projeto --}}
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">

714d8a59871d4d2130a673f527306892b9193bc6
JoaoALD <joaovictorald@gmail.com>
2025-09-18 15:39:42 -0300

    att main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
 
     {{-- Bootsrap CSS TEMPORÁRIO (site) --}}
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> -->
-    
+
     {{-- Bootstrap CSS (local) --}}
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrapLink.css') }}">
 
@@ -24,7 +24,7 @@
 <body>
 
     {{-- NAVBAR aqui ou em @include('layouts.header') --}}
-    <nav class="navbar navbar-expand-lg bs-primary">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid text-white bg-nav nav-h">
             <a class="navbar-brand" href="#">
                 <img src="{{ asset('assets/img/LogoReduzida.svg') }}" alt="LogoFonteNova">

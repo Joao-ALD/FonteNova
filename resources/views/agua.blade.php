93c8762a76b0ccc4ddf29e543697347570171eec
richardLuidy <richard.lrgusansque@gmail.com>
2025-09-19 14:31:01 -0300

    commit

--- resources/views/agua.blade.php
+++ resources/views/agua.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container text-center my-5">
+  <div class="container text-center my-5">
 
     <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <h1 class="display-4 fw-bold">Tudo sobre a Água</h1>
-            <p class="lead">
-                Explore o mundo da Água: do clima à preservação, entenda como que cada ação impacta o nosso recurso mais precioso.
-            </p>
-        </div>
+      <div class="col-lg-8">
+        <h1 class="display-4 fw-bold">Tudo sobre a Água</h1>
+        <p class="lead">
+          Explore o mundo da Água: do clima à preservação, entenda como que cada ação impacta o nosso recurso mais
+          precioso.
+        </p>
+      </div>
     </div>
+  </div>
 
-   
 @endsection

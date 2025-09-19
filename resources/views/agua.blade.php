6c9a58c50fb9f3a436268aa99781ebccaf0bb604
richardLuidy <richard.lrgusansque@gmail.com>
2025-09-19 15:59:25 -0300

    commit

--- resources/views/agua.blade.php
+++ resources/views/agua.blade.php
@@ -42,8 +42,8 @@
     <div class="col-12">
       <div class="card text-start card text-bg-info mb-3" style="width: 100%; height: 20rem;">
         <div class="card-body">
-          <h5 class="card-title text-white text-end">Card de Clima 1</h5>
-          <p class="card-text text-white text-center">Aqui vai a informação sobre o clima. Este é o primeiro card.</p>
+          <h5 class="card-title text-white text-end">Clima</h5>
+          <p class="card-text text-white text-center">O clima desempenha um papel fundamental na disponibilidade de água no planeta. Mudanças climáticas intensificam eventos extremos como secas prolongadas e enchentes, afetando diretamente rios, lagos e aquíferos subterrâneos. Regiões com chuvas irregulares enfrentam escassez, enquanto áreas com excesso de precipitação correm risco de inundações. Assim, o clima é o primeiro fator que determina a quantidade e a qualidade da água disponível.</p>
         </div>
       </div>
     </div>
@@ -52,7 +52,7 @@
       <div class="card text-start card text-bg-info mb-3" style="width: 100%; height: 20rem;">
         <div class="card-body">
           <h5 class="card-title text-white text-end">Clima</h5>
-          <p class="card-text text-white text-center">O clima desempenha um papel fundamental na disponibilidade de água no planeta. Mudanças climáticas intensificam eventos extremos como secas prolongadas e enchentes, afetando diretamente rios, lagos e aquíferos subterrâneos. Regiões com chuvas irregulares enfrentam escassez, enquanto áreas com excesso de precipitação correm risco de inundações. Assim, o clima é o primeiro fator que determina a quantidade e a qualidade da água disponível.</p>
+          <p class="card-text text-white text-center">Aqui vai a informação sobre o clima. Este é o primeiro card</p>
         </div>
       </div>
     </div>

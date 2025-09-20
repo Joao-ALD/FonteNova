6ab20894880db07bdf18f55884690a815b3bec17
Joao-ALD <joaovictorald@gmail.com>
2025-09-20 20:24:07 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/infoAgua', [AguaController::class, 'index'])->name('agua.index');
+Route::get('/ChatBot', [ChatBotController::class, 'index'])->name('chatbot.index');

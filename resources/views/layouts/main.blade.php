0358fbd63355165fd53d220691a6c795ea2011f7
JoaoALD <joaovictorald@gmail.com>
2025-09-23 13:49:31 -0300

    teste

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -66,6 +66,8 @@
         @yield('content')
     </main>
 
+    
+
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('assets/js/script.js') }}"></script>

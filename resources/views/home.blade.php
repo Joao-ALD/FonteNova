b90810600bdb543e1f82caab61238ca2ba3e4e0f
JoaoALD <joaovictorald@gmail.com>
2025-09-25 17:18:06 -0300

    consertando card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('title', 'FonteNova - Início')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
 @endpush
 
 @section('content')
@@ -35,23 +35,25 @@
         <div class="row g-4 justify-content-center">
             @php
             $cards = [
-                ['img' => 'icon_balde.svg', 'label' => 'Coleta de chuva', 'bg' => '#3b8eed'],
-                ['img' => 'icon_water.svg', 'label' => 'Reuso de água', 'bg' => '#014ba0'],
-                ['img' => 'icon_hands.svg', 'label' => 'Cuidados à água', 'bg' => '#3b8eed'],
-                ['img' => 'icon_filter.svg', 'label' => 'Filtros naturais', 'bg' => '#014ba0'],
+            ['img' => 'icon_balde.svg', 'label' => 'Coleta de chuva', 'bg' => '#3b8eed'],
+            ['img' => 'icon_water.svg', 'label' => 'Reuso de água', 'bg' => '#014ba0'],
+            ['img' => 'icon_hands.svg', 'label' => 'Cuidados à água', 'bg' => '#3b8eed'],
+            ['img' => 'icon_filter.svg', 'label' => 'Filtros naturais', 'bg' => '#014ba0'],
             ];
             @endphp
             @foreach ($cards as $card)
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-card d-flex flex-column justify-content-center align-items-center p-4 rounded-3 text-white" style="background-color: {{ $card['bg'] }};">
-                        <img src="{{ asset('assets/img/' . $card['img']) }}" alt="{{ $card['label'] }}" class="gallery-card-icon mb-3">
-                        <span class="gallery-card-label fw-bold">{{ $card['label'] }}</span>
-                    </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 d-flex justify-content-center">
+                <div class="gallery-card square-card d-flex flex-column justify-content-center align-items-center p-4 rounded-3 text-white"
+                    style="background-color: {{ $card['bg'] }};">
+                    <img src="{{ asset('assets/img/' . $card['img']) }}" alt="{{ $card['label'] }}" class="gallery-card-icon mb-3">
+                    <span class="gallery-card-label fw-bold">{{ $card['label'] }}</span>
                 </div>
+            </div>
             @endforeach
         </div>
     </x-container>
 </section>
+
 
 <section class="map-section text-white">
     <x-container>
@@ -76,24 +78,23 @@
         <div class="row g-4 justify-content-center">
             @php
             $eduItems = [
-                ['img' => 'icon_workshop.svg', 'label' => 'Oficinas', 'highlight' => false],
-                ['img' => 'icon_ebook.svg', 'label' => 'E-Books', 'highlight' => true],
-                ['img' => 'icon_eventos.svg', 'label' => 'Eventos', 'highlight' => false],
+            ['img' => 'icon_workshop.svg', 'label' => 'Oficinas', 'highlight' => false],
+            ['img' => 'icon_ebook.svg', 'label' => 'E-Books', 'highlight' => true],
+            ['img' => 'icon_eventos.svg', 'label' => 'Eventos', 'highlight' => false],
             ];
             @endphp
             @foreach ($eduItems as $item)
-                <div class="col-lg-4 col-md-6">
-                    <div class="card h-100 border-0 shadow-sm text-center edu-card {{ $item['highlight'] ? 'edu-card--highlighted' : '' }}">
-                        <div class="card-body d-flex flex-column justify-content-center align-items-center p-4">
-                            <img src="{{ asset('assets/img/' . $item['img']) }}" alt="{{ $item['label'] }}" class="edu-card-icon mb-3">
-                            <h4 class="edu-card-label fw-bold">{{ $item['label'] }}</h4>
-                        </div>
-                    </div>
+            <div class="col-lg-4 col-md-6 d-flex justify-content-center">
+                <div class="card square-card border-0 shadow-sm text-center edu-card d-flex flex-column justify-content-center align-items-center p-4 {{ $item['highlight'] ? 'edu-card--highlighted' : '' }}">
+                    <img src="{{ asset('assets/img/' . $item['img']) }}" alt="{{ $item['label'] }}" class="edu-card-icon mb-3">
+                    <h4 class="edu-card-label fw-bold">{{ $item['label'] }}</h4>
                 </div>
+            </div>
             @endforeach
         </div>
     </x-container>
 </section>
+
 
 <section class="section quote-section">
     <x-container>

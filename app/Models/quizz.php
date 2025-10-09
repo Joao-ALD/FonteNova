b6999ed8a16e7b828e469810dbf305a1dbbff2b3
JoaoALD <joaovictorald@gmail.com>
2025-10-09 14:52:50 -0300

    reativando Hasfactory nos models vazios para evitar erros

--- app/Models/quizz.php
+++ app/Models/quizz.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class quizz extends Model
+class Quizz extends Model
 {
-   
+    use HasFactory;
 }

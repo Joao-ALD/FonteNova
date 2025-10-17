a376b0304a87766ced0f22b0fb659ed37be1e68d
JoaoALD <joaovictorald@gmail.com>
2025-10-17 16:18:55 -0300

    att mapa-interativo - corrigindo erros

--- app/Models/Iniciativa.php
+++ app/Models/Iniciativa.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Iniciativa extends Model
 {
     // use HasFactory;
-    
+    protected $fillable = [
+        'estado_sigla',
+        'titulo',
+        'descricao',
+    ];
 }

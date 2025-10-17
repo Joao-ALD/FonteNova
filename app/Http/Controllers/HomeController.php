d6eaec8cac1106dbee25615aaba21a3dece5920c
JoaoALD <joaovictorald@gmail.com>
2025-10-17 15:44:25 -0300

    criando o mapa-interativo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Iniciativa;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view('home');
+public function index()
+    {
+        // 1. Busca todas as iniciativas do banco de dados
+        $iniciativas = Iniciativa::all();
+
+        // 2. Formata os dados para o JavaScript
+        $dadosIniciativasParaMapa = [];
+        foreach ($iniciativas as $iniciativa) {
+            $dadosIniciativasParaMapa[$iniciativa->estado_sigla] = [
+                'titulo' => $iniciativa->titulo,
+                'descricao' => $iniciativa->descricao,
+            ];
+        }
+
+        // 3. Envia os dados para a view da home, junto com outras variáveis que você possa ter
+        return view('home', [ // ou o nome da sua view principal: welcome, index, etc.
+            'dadosIniciativasJson' => json_encode($dadosIniciativasParaMapa)
+        ]);
     }
 }

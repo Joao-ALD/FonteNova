c75aaa15e0576ce412bac89377dc9a0ebcc2a8dc
MarceloViniciusMF <marcelovmf04@gmail.com>
2025-11-05 17:09:42 -0300

    pagina cursos pronta

--- resources/views/curso.blade.php
+++ resources/views/curso.blade.php
@@ -2,51 +2,62 @@
 
 @section('title', 'FonteNova - Curso')
 
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('assets/css/cursos.css') }}">
+@endpush
+
 @section('content')
-<div class="container fluid">
+<div class="container mt-5 mb-5">
     <div class="row">
-        <div class="col-md-2 text-white p-1 vh-50 d-flex flex-column">
-            <button class="btn btn-dark w-100">
-                <i class="bi bi-chevron-right"></i>
-            </button>
 
-            <div class="list-group list-group-flush mt-2">
-                @for ($i = 1; $i <= 24; $i++)
-                <a href="#" class="list-group-item list-group-item-action bg-AzulClaro text-white d-flex justify-content-between align-items-center hover">
-                Aula {{$i}}
-                <i class="bi bi-chevron-right"></i>
+        <div class="col-md-4">
+            <h3 class="mb-3">Módulos do Curso</h3>
+            <div class="list-group" id="aulas-list">
+                <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    Aula 1: Manejo e Conservação do Solo
                 </a>
-                @endfor
-            </div>
+                <a href="#" class="list-group-item list-group-item-action">
+                    Aula 2: Reúso de Água na Prática
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    Aula 3: Construindo Filtros Naturais
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    Aula 4: Coleta de Chuva Eficiente
+                </a>
+                </div>
         </div>
-        <!-- Conteúdo principal -->
-        <div class="col-md-10 p-5">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
-            </p>
 
-            <!-- Vídeo -->
-            <div class="ratio ratio-16x9 mb-4">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/xdYuEzergvE?si=ehcNs531hTU0aYwO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div class="col-md-8">
+            <h2 class="mb-3">Aula 1: Manejo e Conservação do Solo e da Água</h2>
+
+            <div class="ratio ratio-16x9 mb-4" style="border-radius: 8px; overflow: hidden;">
+                <iframe 
+                    src="https://www.youtube.com/embed/SEU_7n94E5A" title="Manejo e Conservação do Solo e da Água" 
+                    allowfullscreen>
+                </iframe>
             </div>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
-            </p>
+            <h4>Sobre esta aula</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
 
             <ul>
                 <li>Lorem Ipsum Dolor</li>
                 <li>Lorem Ipsum Dolor</li>
                 <li>Lorem Ipsum Dolor</li>
-                <li>Lorem Ipsum Dolor</li>
-                <li>Lorem Ipsum Dolor</li>
             </ul>
 
-            <div class="text-end mt-4">
-                <button class="btn text-white bg-AzulClaro">Próximo</button>
+            <hr class="my-4">
+            <div class="d-flex justify-content-between">
+                <a href="#" class="btn btn-outline-secondary">
+                    &larr; Aula Anterior
+                </a>
+                <a href="#" class="btn btn-primary" id="btn-proximo">
+                    Próxima Aula &rarr;
+                </a>
             </div>
         </div>
-     </div>
+
     </div>
 </div>
 @endsection
